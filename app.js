0e4878537517deda3db4e64263cd7616b5e09ca5
vladZah403 <56314316+vladZah403@users.noreply.github.com>
2021-01-30 23:14:51 +0200

    block card

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 let stepbl1 = document.getElementById("stepbl1");
 let stepbrdr1 = document.getElementById("stepbrdr1");
 
+let stepbl2 = document.getElementById("stepbl2");
+let stepbrdr2 = document.getElementById("stepbrdr2");
+
+let stepbl3 = document.getElementById("stepbl3");
+let stepbrdr3 = document.getElementById("stepbrdr3");
+
 stepbl1.onmouseover = function(){
     stepbrdr1.style.borderBottom = "3px solid white"
 }
 
 stepbl1.onmouseout = function(){
     stepbrdr1.style.borderBottom = "3px solid black"
+}
+
+stepbl2.onmouseover = function(){
+    stepbrdr2.style.borderBottom = "3px solid white"
+}
+
+stepbl2.onmouseout = function(){
+    stepbrdr2.style.borderBottom = "3px solid black"
+}
+
+stepbl3.onmouseover = function(){
+    stepbrdr3.style.borderBottom = "3px solid white"
+}
+
+stepbl3.onmouseout = function(){
+    stepbrdr3.style.borderBottom = "3px solid black"
 }
 
 
