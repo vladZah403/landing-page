299a2750fc23cb5bd09f691946f940b69a47e061
vladZah403 <56314316+vladZah403@users.noreply.github.com>
2021-03-14 00:06:24 +0200

    modal adaptability

--- script/modal.js
+++ script/modal.js
@@ -1,13 +1,15 @@
-var modal = document.getElementById("my_modal");
- var btn = document.getElementById("btn_modal_window");
- var span = document.getElementsByClassName("close_modal_window")[0];
+  let money = document.getElementById("money"); 
+   money.onclick = function (){
+      money.classList.add("btn_modal_window")
+      modal.style.display = "block";
+   }  
 
- btn.onclick = function () {
-    modal.style.display = "block";
- }
+ let modal = document.getElementById("my_modal");
+ let span = document.getElementsByClassName("close_modal_window")[0];
 
  span.onclick = function () {
     modal.style.display = "none";
+    money.classList.remove("btn_modal_window");
  }
 
  window.onclick = function (event) {
@@ -15,3 +17,4 @@ var modal = document.getElementById("my_modal");
         modal.style.display = "none";
     }
 }
+
