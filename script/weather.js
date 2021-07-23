487b4894b75a4410406cbe5cbe64d7becc57e2f6
vladZah403 <56314316+vladZah403@users.noreply.github.com>
2021-07-23 21:46:52 +0300

    card-weather 2.0

--- script/weather.js
+++ script/weather.js
@@ -23,3 +23,24 @@ fetch('https://api.openweathermap.org/data/2.5/weather?lat=48.45&lon=34.9833&app
     .catch(function () {
         
     });
+
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=40.7143&lon=-74.006&appid=3a4fd0c6c98e54f048cee6795700f961').then(function (resp) {return resp.json() }).then(function (data) {
+    document.querySelector('.weather__city3').textContent = data.name;
+    document.querySelector('.weather__forecast3').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
+    document.querySelector('.weather__desc3').textContent = data.weather[0]['description'];
+    document.querySelector('.weather__icon3').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+    })
+    .catch(function () {
+        
+    });
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=50.0883&lon=14.4208&appid=3a4fd0c6c98e54f048cee6795700f961').then(function (resp) {return resp.json() }).then(function (data) {
+    document.querySelector('.weather__city4').textContent = data.name;
+    document.querySelector('.weather__forecast4').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
+    document.querySelector('.weather__desc4').textContent = data.weather[0]['description'];
+    document.querySelector('.weather__icon4').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+    })
+    .catch(function () {
+        
+    });
