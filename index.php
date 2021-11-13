8cfdfeb10464fbc7fd270fee37fb9b415eed1f21
vladZah403 <56314316+vladZah403@users.noreply.github.com>
2021-11-13 21:21:32 +0200

    php

--- index.php
+++ index.php
@@ -1,17 +1,40 @@
 <?php
 
- $url = "https://api.openweathermap.org/data/2.5/weather?id=498817&units=metric&appid=3a4fd0c6c98e54f048cee6795700f961&lang=ru";
 
- $contents = file_get_contents($url);
- $weather=json_decode($contents);
- 
- $temp_now=$weather->main->temp."°C";
- $icon=$weather->weather[0]->icon.".png";
- $today = date("j.m.Y, H:i");
- $cityname = $weather->name;
- 
- echo $today."<br />".
- $cityname."<br />".
- $temp_now."<br />
- <img src='./icons/" . $icon ."' class='condition'/>";
+//В переменную $token нужно вставить токен, который нам прислал @botFather
+$token = "2118395924:AAEueigq0y6R9CmEbsaW1Lwq1rPA5ly9WUM";
+
+//Сюда вставляем chat_id
+$chat_id = "-1001658073520";
+
+//Определяем переменные для передачи данных из нашей формы
+if ($_POST['act'] == 'order') {
+    $name = ($_POST['name']);
+    $phone = ($_POST['phone']);
+
+//Собираем в массив то, что будет передаваться боту
+    $arr = array(
+        'Имя:' => $name,
+        'Телефон:' => $phone
+    );
+
+//Настраиваем внешний вид сообщения в телеграме
+    foreach($arr as $key => $value) {
+        $txt .= "<b>".$key."</b> ".$value."%0A";
+    };
+
+//Передаем данные боту
+    $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+//Выводим сообщение об успешной отправке
+    if ($sendToTelegram) {
+        alert('Спасибо! Ваша заявка принята. Мы свяжемся с вами в ближайшее время.');
+    }
+
+//А здесь сообщение об ошибке при отправке
+    else {
+        alert('Что-то пошло не так. ПОпробуйте отправить форму ещё раз.');
+    }
+}
+
 ?>
