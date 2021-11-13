d5c6863f8e9adda6a1444ec0368597de2a6f0864
vladZah403 <56314316+vladZah403@users.noreply.github.com>
2021-11-14 00:11:00 +0200

    messegeTelegram

--- index.php
+++ index.php
@@ -1,37 +1,27 @@
 <?php
-
-
-//В переменную $token нужно вставить токен, который нам прислал @botFather
+include 'index.html';
 $token = "2118395924:AAEueigq0y6R9CmEbsaW1Lwq1rPA5ly9WUM";
-
-//Сюда вставляем chat_id
 $chat_id = "-1001658073520";
 
-//Определяем переменные для передачи данных из нашей формы
 if ($_POST['act'] == 'order') {
     $name = ($_POST['name']);
-    $phone = ($_POST['phone']);
+    $text = ($_POST['text']);
 
-//Собираем в массив то, что будет передаваться боту
+
     $arr = array(
         'Имя:' => $name,
-        'Телефон:' => $phone
+        'сообщение:' => $text,
     );
 
-//Настраиваем внешний вид сообщения в телеграме
     foreach($arr as $key => $value) {
         $txt .= "<b>".$key."</b> ".$value."%0A";
     };
 
-//Передаем данные боту
     $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
-//Выводим сообщение об успешной отправке
     if ($sendToTelegram) {
         alert('Спасибо! Ваша заявка принята. Мы свяжемся с вами в ближайшее время.');
     }
-
-//А здесь сообщение об ошибке при отправке
     else {
         alert('Что-то пошло не так. ПОпробуйте отправить форму ещё раз.');
     }
