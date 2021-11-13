76bcdc111e39d598269ce425f80db0aaba714e3f
vladZah403 <56314316+vladZah403@users.noreply.github.com>
2021-11-13 20:09:26 +0200

    registr 2.0

--- script/registration.js
+++ script/registration.js
@@ -2,12 +2,17 @@ const login = document.querySelector('#login');
 const RegistrForm = document.querySelector('#reg');
 const Registr = document.querySelector('#Registr')
 const loginForm = document.querySelector('#log')
-const batRegistr = document.querySelector('.registr')
+let batRegistr = document.querySelector('.registr')
 const Main = document.querySelector('#main')
 const mainRegistr = document.querySelector('#mainRegistr')
 const message = document.querySelector('.message')
 
 
+const at = document.querySelector('#at')
+const key = document.querySelector('#key')
+const user = document.querySelector('#user')
+
+const Exit = document.querySelector(".Exit")
 
 batRegistr.addEventListener('click', () => {
     Main.style.display = 'none'
@@ -53,28 +58,53 @@ const submitReg = document.querySelector('.submitReg')
 
 const mainPersonal = document.querySelector('#mainPersonal')
 
+console.log(batRegistr.classList.contains('registr'));
+
 submitReg.addEventListener('click', () => {
     console.log(UserReg.match(regUser));
     console.log(UserPas.match(regPassword));
     console.log(UserMail.match(regMail))
-    if (UserReg.match(regUser) !== null && UserPas.match(regPassword) !== null && UserMail.match(regMail) !== null) {
-        localStorage.setItem(`${UserReg}`, `${UserPas} / ${UserMail}`);
+
+    let userProver = JSON.parse(localStorage.getItem(UserReg))
+
+    console.log(userProver);
+    
+    if(userProver !== null ){
+       console.log(userProver)
+       message.innerHTML =' User with this username already exists'
+    }else if (  UserReg && UserReg.match(regUser) !== null && UserPas.match(regPassword) !== null && UserMail.match(regMail) !== null) {
 
         carentUser.Login = UserReg;
         carentUser.UserPassword = UserPas;
         carentUser.UserEmail = UserMail
 
+        let objUser = JSON.stringify(carentUser)
+
+        localStorage.setItem(`${UserReg}`, `${objUser}`);
+
+        
+
         console.log(carentUser);
 
         Main.style.display = ''
         mainRegistr.style.display = 'none'
+        mainPersonal.style.display = 'none'
 
 
         batRegistr.innerHTML = '<i class="far fa-user fa-lg  icon-user"></i>'
-        batRegistr.classList.add('userBox');
-        batRegistr.classList.remove('registr');
+
         message.innerHTML = ''
 
+        batRegistr.classList.remove('registr');
+        batRegistr.classList.add('userBox');
+        batRegistr = document.querySelector('.userBox')
+
+
+        user.innerHTML = ` ${UserReg}`
+
+        key.innerHTML = ` ${UserPas}`
+
+        at.innerHTML = ` ${UserMail}`
 
     } else if (UserReg.match(regUser) == null) {
         message.innerHTML = 'You entered an invalid username'
@@ -82,27 +112,73 @@ submitReg.addEventListener('click', () => {
         message.innerHTML = 'You entered an incorrect password'
     } else if (UserMail.match(regMail) == null) {
         message.innerHTML = 'You entered an invalid E-mail'
-    }
+    } 
+
 })
 
 
+const logo = document.querySelector('.logo')
 
-// const userBox = document.querySelector('.userBox')
-// if (userBox.classList.contains('userBox')) {
-//     userBox.addEventListener('click', () => {
-//         mainPersonal.style.display = ''
-//         Main.style.display = 'none'
-//         mainRegistr.style.display = 'none'
-//     })
-// }
+batRegistr.addEventListener('click', () => {
+    if (batRegistr.classList.contains('userBox') !== false) {
+
+        mainPersonal.style.display = ''
+        Main.style.display = 'none'
+        mainRegistr.style.display = 'none'
+
+    }
+})
+
+logo.addEventListener('click', () => {
+    mainPersonal.style.display = 'none'
+    Main.style.display = ''
+    mainRegistr.style.display = 'none'
+})
+
+Exit.addEventListener('click', () =>{
+    window.location.href = '../index.html';
+})
+
+let nameLog = ''
+let pasLog =' '
+
+const submitSign = document.querySelector('.submitSign')
+
+document.querySelector('.Us_Log').addEventListener('input', () => {
+    nameLog = document.querySelector('.Us_Log').value;
+})
+
+document.querySelector('.Pas_Log ').addEventListener('input', () => {
+    pasLog = document.querySelector(' .Pas_Log ').value;
+})
+
+submitSign.addEventListener('click', () =>{
+    console.log(nameLog);
+    console.log(pasLog);
+    let UserStopeg = JSON.parse(localStorage.getItem(nameLog))
+    
+    if(nameLog === UserStopeg.Login && pasLog === UserStopeg.UserPassword){
+
+        carentUser.Login = UserStopeg.Login;
+        carentUser.UserPassword = UserStopeg.UserPassword;
+        carentUser.UserEmail = UserStopeg.UserEmail
+
+        Main.style.display = ''
+        mainRegistr.style.display = 'none'
+        mainPersonal.style.display = 'none'
 
 
+        batRegistr.innerHTML = '<i class="far fa-user fa-lg  icon-user"></i>'
 
+        batRegistr.classList.remove('registr');
+        batRegistr.classList.add('userBox');
 
-const user = document.querySelector('#user')
-const key = document.querySelector('#key')
-const at = document.querySelector('#at')
+        console.log(carentUser)
+        user.innerHTML = ` ${UserStopeg.Login}`
 
+        key.innerHTML = ` ${UserStopeg.UserPassword}`
 
-
+        at.innerHTML = ` ${UserStopeg.UserEmail}`
+    }
+})
 
